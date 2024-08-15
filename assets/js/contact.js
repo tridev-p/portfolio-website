a3cbb4b0696c3ee0689a66b85789a21aac6d63d6
3devdev <tridevchowdary@icloud.com>
2024-08-15 00:47:02 -0500

    code of portfolio

---
+++ assets/js/contact.js
@@ -0,0 +1,28 @@
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_emwpd85', 'template_9meyhln', '#contact-form', 'uFhW_WHzkjWBXiPnd')
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅'
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            // Clear input fields
+            contactForm.reset()
+
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
